fc86b7c600ec0515a787efd25b0d8da1b381480a
geansru <d@geans.ru>
2014-06-21 14:47:34 +0600

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,20 @@
 class StaticPagesController < ApplicationController
+  before_action :set_title
   def home
+    set_title 'Home'
   end
 
   def help
+    set_title 'Help'
+  end
+
+  def about
+    set_title 'About'
+  end
+
+  private
+  def set_title title_name=nil
+    @title = "Ruby on Rails Tutorial Sample App"
+    @title += " | #{title_name}" if title_name
   end
 end
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", :type => :request do
+  describe "GET /static_pages" do
+    it "It should have content 'Sample App'" do
+      visit static_pages_home_path
+      expect(page).to have_content('Sample App')
+    end
+    it "should have a Help content" do
+      visit static_pages_help_path
+      expect(page).to have_content('Help page')
+    end
+
+    describe "About page" do
+      it "should have content 'About Us'" do
+        visit static_pages_about_path
+        expect(page).to have_content('About Us')
+      end
+    end
+  end
+
+  describe "All pages must have a title" do
+    it 'About page should have a title' do
+      visit static_pages_about_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it 'About page should have a specified title' do
+      visit static_pages_about_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+    
+    it 'Home page should have a specified title' do
+      visit static_pages_home_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+    
+    it 'Home page should have a title' do
+      visit static_pages_home_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it 'Help page should have a title' do
+      visit static_pages_help_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+    
+    it 'Help page should have a title' do
+      visit static_pages_help_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+end
