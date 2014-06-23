65eb0e6ceae8ec0227893c0c7fe1c9c03e80a5e5
geansru <d@geans.ru>
2014-06-23 20:49:47 +0600

    modified:   spec/requests/static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,33 +4,37 @@ require 'support/utilities'
 RSpec.describe "StaticPages", :type => :request do
   @title = ''
   let(:title) { "Ruby on Rails Tutorial Sample App" }
-
+  subject { page }
   context "Home page" do
     before { visit root_path }
-    it { page.should have_content('Sample App') }
-    it { page.should have_title(full_title "Home") }
-    it { page.should have_title(full_title) }
+    it { should have_content('Sample App') }
+    it { should have_selector('h2', 'Sample App') }
+    it { should have_title(full_title "Home") }
+    it { should have_title(full_title) }
   end
 
   context "About page" do
     before { visit about_path }
-    it { page.should have_content('About Us') }
-    it { page.should have_title(full_title "About") }
-    it { page.should have_title(full_title) }
+    it { should have_content('About Us') }
+    it { should have_selector('h2', 'About Us') }
+    it { should have_title(full_title "About") }
+    it { should have_title(full_title) }
   end
 
   context "Help page" do 
     before { visit help_path }
-    it { page.should have_content('Help page') }
-    it { page.should have_title(full_title) }
-    it { page.should have_title(full_title "Help") }
+    it { should have_content('Help page') }
+    it { should have_selector('h2', 'Help page') }
+    it { should have_title(full_title) }
+    it { should have_title(full_title "Help") }
   end
 
   context "Contacts page" do
     before { visit contacts_path }
-    it { page.should have_content("Contacts")}
-    it { page.should have_title(full_title) }
-    it { page.should have_title(full_title "Contacts") }
+    it { should have_content("Contacts")}
+    it { should have_selector('h2', text: "Contacts") }
+    it { should have_title(full_title) }
+    it { should have_title(full_title "Contacts") }
   end
 
 end
