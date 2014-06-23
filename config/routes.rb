b9ebf5b3d2017098153960610d45f8d67d4e8412
geansru <d@geans.ru>
2014-06-23 17:09:51 +0600

    Add sigin/signup to navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-  get 'static_pages/contacts'
+  get 'static_pages/home', as: 'home'
+  get 'static_pages/help', as: 'help'
+  get 'static_pages/about', as: 'about'
+  get 'static_pages/contacts', as: 'contacts'
+  get 'static_pages/signin', as: 'signin'
+  get 'static_pages/signup', as: 'signup'
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
