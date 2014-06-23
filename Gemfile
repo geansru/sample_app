e58b9383d2fdccc9455c05233310e109b4f98713
geansru <d@geans.ru>
2014-06-23 17:26:24 +0600

    modified:   .rspec 	modified:   Gemfile 	modified:   Gemfile.lock 	modified:   spec/requests/static_pages_spec.rb

--- Gemfile
+++ Gemfile
@@ -3,16 +3,18 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.1.1'
+
 # Use sqlite3 as the database for Active Record
 group :development, :test do
   gem 'sqlite3'
   gem 'rspec-rails'
   gem 'guard-rspec'
+  gem 'libnotify'
 end
 group :test do
   gem 'selenium-webdriver'
   gem 'capybara'
-  gem 'libnotify', '0.8.0'
+  gem 'libnotify'#, '0.8.0'
 end
 # Use SCSS for stylesheets
 gem 'sass-rails'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,24 +5,24 @@ RSpec.describe "StaticPages", :type => :request do
   let(:title) { "Ruby on Rails Tutorial Sample App" }
   describe "GET /static_pages" do
     it "It should have content 'Sample App'" do
-      visit static_pages_home_path
+      visit home_path
       expect(page).to have_content('Sample App')
     end
     it "should have a Help content" do
-      visit static_pages_help_path
+      visit help_path
       expect(page).to have_content('Help page')
     end
 
     describe "About page" do
       it "should have content 'About Us'" do
-        visit static_pages_about_path
+        visit about_path
         expect(page).to have_content('About Us')
       end
     end
 
     describe "Contacts page" do
       it "should have content 'Contacts'" do
-        visit static_pages_contacts_path
+        visit contacts_path
         expect(page).to have_content("Contacts")
         expect(page).to have_title(@title)
         expect(page).to have_title("#{@title} | Contacts")
@@ -32,32 +32,32 @@ RSpec.describe "StaticPages", :type => :request do
 
   describe "All pages must have a title" do
     it 'About page should have a title' do
-      visit static_pages_about_path
+      visit about_path
       expect(page).to have_title(@title)
     end
     
     it 'About page should have a specified title' do
-      visit static_pages_about_path
+      visit about_path
       expect(page).to have_title("#{@title} | About")
     end
     
     it 'Home page should have a specified title' do
-      visit static_pages_home_path
+      visit home_path
       expect(page).to have_title("#{@title} | Home")
     end
     
     it 'Home page should have a title' do
-      visit static_pages_home_path
+      visit home_path
       expect(page).to have_title(@title)
     end
 
     it 'Help page should have a title' do
-      visit static_pages_help_path
+      visit help_path
       expect(page).to have_title(@title)
     end
     
     it 'Help page should have a title' do
-      visit static_pages_help_path
+      visit help_path
       expect(page).to have_title("#{@title} | Help")
     end
   end
