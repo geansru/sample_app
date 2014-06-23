61d4cf68efb1be375d4c59e57cf0c1145fb837f5
geansru <d@geans.ru>
2014-06-23 21:13:31 +0600

    Fix navigation menu errors

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,36 +5,60 @@ RSpec.describe "StaticPages", :type => :request do
   @title = ''
   let(:title) { "Ruby on Rails Tutorial Sample App" }
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(content) }
+    it { should have_selector('h2', text: content) }
+    it { should have_title(full_title title) }
+    it { should have_title(full_title) }
+  end
   context "Home page" do
     before { visit root_path }
-    it { should have_content('Sample App') }
-    it { should have_selector('h2', 'Sample App') }
-    it { should have_title(full_title "Home") }
-    it { should have_title(full_title) }
+    let(:content) { 'Sample App' }
+    let(:title) { "Home" }
+
+    it_should_behave_like "all static pages"
   end
 
   context "About page" do
     before { visit about_path }
-    it { should have_content('About Us') }
-    it { should have_selector('h2', 'About Us') }
-    it { should have_title(full_title "About") }
-    it { should have_title(full_title) }
+
+    let(:content) { 'About Us' }
+    let(:title) { "About"  }
+    it_should_behave_like "all static pages"
   end
 
   context "Help page" do 
     before { visit help_path }
-    it { should have_content('Help page') }
-    it { should have_selector('h2', 'Help page') }
-    it { should have_title(full_title) }
-    it { should have_title(full_title "Help") }
+    let(:content) { 'Help page' }
+    let(:title) { 'Help' }
+    it_should_behave_like "all static pages"
   end
 
   context "Contacts page" do
     before { visit contacts_path }
-    it { should have_content("Contacts")}
-    it { should have_selector('h2', text: "Contacts") }
-    it { should have_title(full_title) }
-    it { should have_title(full_title "Contacts") }
+    let(:content) { "Contacts" }
+    let(:title) { "Contacts" }
+    it_should_behave_like "all static pages"
   end
 
+  context "Layout navigation" do
+    it "should have valid  links on the layout" do
+      visit root_path
+      click_link "About"
+      expect(page).to have_title(full_title("About"))
+
+      click_link "Help"
+      expect(page).to have_title(full_title("Help"))
+
+      click_link "Contacts"
+      expect(page).to have_title(full_title("Contacts"))
+
+      click_link "Sign up"
+      expect(page).to have_content("Sign up now")
+
+      click_link "Sample App"
+      expect(page).to have_title(full_title("Home"))
+    end
+  end
 end
