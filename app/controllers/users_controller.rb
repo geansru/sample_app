f00eadcb4ff4b983cff312eefac9d807f87f55d4
geansru <d@geans.ru>
2014-06-23 20:41:26 +0600

    Add title to users#new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_title
   def new
+    set_title "Sign up"
+  end
+
+  def set_title custom=nil
+    @title = "Ruby on Rails Tutorial Sample App"
+    @title += " | " + custom if custom
+    @title 
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "UserPages", :type => :request do
+  subject { page }
   describe "View page" do
     before { visit signup_path }
-    it { page.should have_content("Sign up now") }
+    it { should have_content("Sign up now") }
+    it { should have_title(full_title) }
+    it { should have_title(full_title "Sign up") }
 #    it { expect(response.status).to be(200) }
 
   end
